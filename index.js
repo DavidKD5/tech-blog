466f786aecc74c4d420fb48b78ddc998e73a74d7
DavidKD5 <jdbetancourt6@gmail.com>
2022-08-27 17:04:45 -0600

    html and css models 85% complete

---
+++ index.js
@@ -0,0 +1,13 @@
+const writeComment = $(".blogPostComment");
+
+$(".commentSection").hide();
+
+writeComment.click(() => {
+  $(".singlePost").css("filter", "blur(2px)");
+  $(".commentSection").show();
+});
+
+$(".ex").click(() => {
+  $(".commentSection").hide();
+  $(".singlePost").css("filter", "");
+});
